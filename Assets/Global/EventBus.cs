276c18ad1fa9884305ad5faae13026fed0d28669
Stanislav Rassolenko <Stanislav.Rassolenko@gmail.com>
2016-08-27 18:22:47 +0300

    Initial Commit

---
+++ Assets/Global/EventBus.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.Events;
+
+public class EventBus : MonoBehaviour {
+
+	static event Action<Card> OnCardClickedEvent;
+
+	// Use this for initialization
+	void Start () {
+		OnCardClickedEvent += OnCardClicked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void OnCardClicked (Card card) {
+		Debug.Log ("HELLO");
+	}
+
+
+}
